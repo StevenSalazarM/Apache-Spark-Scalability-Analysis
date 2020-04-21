db6fe0f3b0fe9689cf8f53606f428dccb26bbb37
Steven Salazar <stevensalazarmolina@gmail.com>
2020-04-21 20:35:09 +0200

    Script on Master: added a bash script that allows to create a specific type and number of EC2 instances from the AWS command line interface.
    Signed-off-by: Steven Salazar <stevensalazarmolina@gmail.com>

---
+++ launch_ec2_instance.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ ( $1 == "" || $2 == "") ]]; then
+        echo "First param is the template name."
+        echo "Second param is the number of instances."
+else
+        # it takes as param $1 launchtemplate name and $2 for the number of instances
+	# before executing this script make sure that you have an AMI of the VM that you would like to clone and also create a template from the Web Console specifying the placement group and the instance type 
+        aws ec2 run-instances   --image-id ami-id --key-name template-id --subnet-id subnet-id --launch-template LaunchTemplateName=$1 --count $2 && echo "$2 Instances with name $1 have been launched, check the web console."
+fi
+
