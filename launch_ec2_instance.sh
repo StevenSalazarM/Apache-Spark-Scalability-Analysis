4446e7caccf9e733c9aa02154b792a5643d148ad
Steven Salazar <stevensalazarmolina@gmail.com>
2020-05-09 20:07:33 +0200

    small modification of the launch_ec2 script
    Signed-off-by: Steven Salazar <stevensalazarmolina@gmail.com>

--- launch_ec2_instance.sh
+++ launch_ec2_instance.sh
@@ -6,6 +6,6 @@ if [[ ( $1 == "" || $2 == "") ]]; then
 else
         # it takes as param $1 launchtemplate name and $2 for the number of instances
 	# before executing this script make sure that you have an AMI of the VM that you would like to clone and also create a template from the Web Console specifying the placement group and the instance type 
-        aws ec2 run-instances   --image-id ami-id --key-name template-id --subnet-id subnet-id --launch-template LaunchTemplateName=$1 --count $2 && echo "$2 Instances with name $1 have been launched, check the web console."
+        aws ec2 run-instances   --image-id ami_id --key-name key_name --subnet-id subnet_id --launch-template LaunchTemplateName=$1 --count $2 && echo "$2 Instances with name $1 have been launched, check the web console if you want to verify the completion of this script."
 fi
 
